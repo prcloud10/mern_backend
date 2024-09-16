dc02ac6726a63815cc1a8ada0cbf8a79a050d865
luis <luis@hp.com>
2024-09-16 16:12:14 -0300

    update k3d

--- scripts/k3d/createcuda.sh
+++ scripts/k3d/createcuda.sh
@@ -20,10 +20,10 @@ log_dir="logs"
 rm -rf "${log_dir:?}/"
 mkdir -p "${log_dir}"
 
-echo "creating local cluster..."
+printf "creating local cluster..."
 
 k3d cluster create local \
-  --config ./cluster.local.yml \
+  --config ./cluster.cuda.local.yml \
   1>"${log_dir}/k3s.log" \
   2>"${log_dir}/k3s.err.log"
 echo " Done!"
@@ -33,7 +33,10 @@ echo
 echo "Cluster is started up!"
 echo
 echo "The following Services are provided out of the box:"
+echo "    https://grafana.k3d.localhost (username: 'admin', password: 'prom-operator')"
+echo "    https://prometheus.k3d.localhost"
 echo "    https://traefik.k3d.localhost"
+echo "    https://local.registry.k3d.localhost"
 echo
 echo "The system provides a registry at"
 echo "    localhost:5000"
